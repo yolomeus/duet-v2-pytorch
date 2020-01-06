564bb6582f14db313a32b22ceb5e9237012175b5
Fabian <fab.beringer@gmail.com>
2020-01-06 19:16:25 +0100

    implement export of candidate sets

--- preprocessing/h5py_savers.py
+++ preprocessing/h5py_savers.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 from config import FiQADatasetConfig
 from preprocessing import tokenizers
-from qa_utils.preprocessing.dataset import Dataset
+from qa_utils.preprocessing.dataset import Dataset, Trainset, Testset
 from qa_utils.preprocessing.fiqa import FiQA
 from util.io import dump_pkl_file
 from util.text import build_vocab, compute_idfs
@@ -38,8 +38,38 @@ class Hdf5Saver(ABC):
         self.train_out, self.dev_out, self.test_out = (h5py.File(fpath, 'w') if fpath else None for fpath in
                                                        out_paths)
 
+    def build_all(self):
+        """Exports each split in dataset to hdf5 if an output file was specified for it.
+        """
+        if self.train_out:
+            self._save_train_set()
+        if self.test_out:
+            self._save_candidate_set('test')
+        if self.dev_out:
+            self._save_candidate_set('dev')
+
+    def _save_candidate_set(self, split):
+        fp, dataset = (self.dev_out, self.dataset.devset) if split == 'dev' else (self.test_out, self.dataset.testset)
+
+        print('saving to', fp.filename, '...')
+        self._define_candidate_set(fp, self._n_out_samples(dataset))
+        self.idx = 0
+
+        for q_id, query, doc, label in tqdm(dataset):
+            processed_row = self._transform_candidate_row(q_id, query, doc, label)
+            self._save_candidate_row(fp, *processed_row)
+
+    def _save_train_set(self):
+        print("saving", self.train_outpath, "...")
+        self._define_trainset(self.train_out, self._n_out_samples(self.dataset.trainset))
+        self.idx = 0
+
+        for query, pos_doc, neg_docs in tqdm(self.dataset.trainset):
+            processed_row = self._transform_train_row(query, pos_doc, neg_docs)
+            self._save_train_row(*processed_row)
+
     @abstractmethod
-    def _define_dataset(self, dataset_fp, n_out_examples):
+    def _define_trainset(self, dataset_fp, n_out_examples):
         """Specify the structure of the hdf5 output file.
 
         Args:
@@ -48,45 +78,56 @@ class Hdf5Saver(ABC):
         """
 
     @abstractmethod
-    def output_size(self, split):
-        """Computes the number of output examples generated for either train or test set.
+    def _define_candidate_set(self, dataset_fp, n_out_examples):
+        """Specify the structure of the hdf5 output file for candidate type sets.
 
         Args:
-            split: either 'train' or 'test'
+            dataset_fp: file pointer to the hdf5 output file.
+            n_out_examples: number of examples that will be generated for this dataset.
+        """
+
+    @abstractmethod
+    def _n_out_samples(self, dataset):
+        """Computes the number of output examples generated for either train, test or dev set.
+
+        Args:
+            dataset: either Trainset or Testset from qa_utils.
 
         Returns:
-            int: number of total output
+            int: number of total output samples.
 
         """
-        assert split in ['train', 'test'], 'can only compute output_size for either "train" or "test".'
 
     @abstractmethod
-    def _transform_train_row(self, *args):
-        pass
+    def _transform_train_row(self, query, pos_doc, neg_docs):
+        """This function is applied to each row in the train set as returned by a qa_utils Trainset before saving.
+
+        Args:
+            query (str): a query string.
+            pos_doc (str): a positive document string w.r.t. the query.
+            neg_docs (list(str)): a list of negative documents.
+
+        Returns:
+            The transformed row.
+        """
 
     @abstractmethod
-    def _transform_test_row(self):
-        pass
+    def _transform_candidate_row(self, q_id, query, doc, label):
+        """This function is applied to each row in the test ord dev set as returned by qa_utils Trainset before saving.
+
+                Args:
+
+                Returns:
+                    The transformed row.
+        """
 
     @abstractmethod
     def _save_train_row(self, *args):
         pass
 
     @abstractmethod
-    def _save_test_row(self):
+    def _save_candidate_row(self, *args):
         pass
-
-    def _save_test_set(self):
-        pass
-
-    def _save_train_set(self):
-        print("saving", self.train_outpath, "...")
-        self._define_dataset(self.train_out, self.output_size('train'))
-        self.idx = 0
-
-        for query, pos_doc, neg_docs in tqdm(self.dataset.trainset):
-            processed_row = self._transform_train_row(query, pos_doc, neg_docs)
-            self._save_train_row(*processed_row)
 
 
 class DuetHhdf5Saver(Hdf5Saver):
@@ -120,7 +161,7 @@ class DuetHhdf5Saver(Hdf5Saver):
         # map token ids to idfs
         self.idfs = dict(map(lambda x: (self.word_to_index[x[0]], x[1]), self.idfs.items()))
 
-    def _define_dataset(self, dataset_fp, n_out_examples):
+    def _define_trainset(self, dataset_fp, n_out_examples):
         vlen_uint32 = h5py.special_dtype(vlen=np.dtype('uint32'))
         dataset_fp.create_dataset('queries', shape=(n_out_examples,), dtype=vlen_uint32)
         dataset_fp.create_dataset('pos_docs', shape=(n_out_examples,), dtype=vlen_uint32)
@@ -130,13 +171,25 @@ class DuetHhdf5Saver(Hdf5Saver):
         dataset_fp.create_dataset('pos_imats', shape=imat_shape, dtype=np.dtype('float32'))
         dataset_fp.create_dataset('neg_imats', shape=imat_shape, dtype=np.dtype('float32'))
 
-    def output_size(self, split):
-        if split == 'train':
-            trainset = self.dataset.trainset
-            # duet trains on triplets i.e. one pairs of (q, pos, neg)
-            return len(trainset.pos_pairs) * trainset.num_neg_examples
-        elif split == 'test':
-            pass
+    def _define_candidate_set(self, dataset_fp, n_out_examples):
+        vlen_uint32 = h5py.special_dtype(vlen=np.dtype('uint32'))
+        dataset_fp.create_dataset('queries', shape=(n_out_examples,), dtype=vlen_uint32)
+        dataset_fp.create_dataset('docs', shape=(n_out_examples,), dtype=vlen_uint32)
+
+        imat_shape = (n_out_examples, self.max_query_len, self.max_doc_len)
+        dataset_fp.create_dataset('imats', shape=imat_shape, dtype=np.dtype('float32'))
+
+        dataset_fp.create_dataset('q_ids', shape=(n_out_examples,), dtype=np.dtype('uint32'))
+        dataset_fp.create_dataset('labels', shape=(n_out_examples,), dtype=np.dtype('uint32'))
+
+    def _n_out_samples(self, dataset):
+        if isinstance(dataset, Trainset):
+            # duet trains on triplets i.e. pairs of (q, pos, neg)
+            return len(dataset.pos_pairs) * dataset.num_neg_examples
+        elif isinstance(dataset, Testset):
+            return len(dataset)
+        else:
+            raise TypeError('Dataset needs to be of type Trainset or Testset.')
 
     def _transform_train_row(self, query, pos_doc, neg_docs):
         q_tokens = self.tokenizer.tokenize(query)
@@ -148,11 +201,11 @@ class DuetHhdf5Saver(Hdf5Saver):
         neg_docs_ids = [self._words_to_index(self.tokenizer.tokenize(neg_doc))[:self.max_doc_len] for neg_doc in
                         neg_docs]
 
-        pos_imat = self._build_interaction_matrix(q_ids, pos_ids, self.idfs)
+        pos_imat = self._build_interaction_matrix(q_ids, pos_ids)
         neg_imats = []
 
         for neg_ids in neg_docs_ids:
-            neg_imat = self._build_interaction_matrix(q_ids, neg_ids, self.idfs)
+            neg_imat = self._build_interaction_matrix(q_ids, neg_ids)
             neg_imats.append(neg_imat)
 
         return q_ids, pos_ids, neg_docs_ids, pos_imat, neg_imats
@@ -168,13 +221,29 @@ class DuetHhdf5Saver(Hdf5Saver):
             fp['neg_imats'][self.idx] = neg_imat
             self.idx += 1
 
-    def _transform_test_row(self):
-        pass
+    def _transform_candidate_row(self, q_id, query, doc, label):
+        q_tokens = self.tokenizer.tokenize(query)
+        q_ids = self._words_to_index(q_tokens)[:self.max_query_len]
 
-    def _save_test_row(self):
-        pass
+        doc_tokens = self.tokenizer.tokenize(doc)
+        doc_ids = self._words_to_index(doc_tokens)[:self.max_doc_len]
 
-    def _build_interaction_matrix(self, q_ids, doc_ids, idfs):
+        imat = self._build_interaction_matrix(q_ids, doc_ids)
+
+        return q_id, q_ids, doc_ids, imat, label
+
+    def _save_candidate_row(self, fp, q_id, q_ids, doc_ids, imat, label):
+        fp['q_ids'][self.idx] = q_id
+
+        fp['queries'][self.idx] = q_ids
+        fp['docs'][self.idx] = doc_ids
+        fp['imats'][self.idx] = imat
+
+        fp['labels'][self.idx] = label
+
+        self.idx += 1
+
+    def _build_interaction_matrix(self, q_ids, doc_ids):
         """Helper method for building a IDF-weighted interaction matrix between a query and document. An entry at (i, j)
         is the IDF of the i-th word in the query if it matches the j-th word in the document or 0 else.
 
@@ -190,7 +259,7 @@ class DuetHhdf5Saver(Hdf5Saver):
             for j in range(len(doc_ids)):
                 cur_qid = q_ids[i]
                 if cur_qid == doc_ids[j]:
-                    m[i, j] = idfs[cur_qid]
+                    m[i, j] = self.idfs[cur_qid]
 
         return m
 
@@ -216,4 +285,4 @@ if __name__ == '__main__':
                            './data/fiqa/train.hdf5',
                            './data/fiqa/dev.hdf5',
                            './data/fiqa/test.hdf5')
-    saver._save_train_set()
+    saver.build_all()
