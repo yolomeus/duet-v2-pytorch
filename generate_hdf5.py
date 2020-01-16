95a52d27701d3a8186b1564964b13732ef039499
Fabian <fab.beringer@gmail.com>
2020-01-16 15:37:18 +0100

    add antique to hdf5 generation script

--- generate_hdf5.py
+++ generate_hdf5.py
@@ -3,16 +3,16 @@ from argparse import ArgumentParser
 
 from preprocessing.hdf5_savers import DuetHhdf5Saver
 from preprocessing.tokenizer import DuetTokenizer
+from qa_utils.preprocessing.antique import Antique
 from qa_utils.preprocessing.fiqa import FiQA
 from qa_utils.preprocessing.insrqa import InsuranceQA
 from qa_utils.preprocessing.msmarco import MSMARCO
-from qa_utils.preprocessing.wpqa import WikiPassageQA
 
 if __name__ == '__main__':
     ap = ArgumentParser()
     ap.add_argument('DATA_DIR', type=str, help='Directory with the raw dataset files.')
     ap.add_argument('OUTPUT_DIR', type=str, help='Directory to store the generated files in.')
-    ap.add_argument('DATA_SET', type=str, choices=['FIQA', 'MSMARCO', 'INSURANCE_QA', 'WIKIPASSAGE_QA'],
+    ap.add_argument('DATA_SET', type=str, choices=['FIQA', 'MSMARCO', 'ANTIQUE'],
                     help='The dataset that will be processed.')
     ap.add_argument('--vocab_size', type=int, default=80000,
                     help='Only use the n most frequent words for the vocabulary.')
@@ -29,20 +29,24 @@ if __name__ == '__main__':
 
     args = ap.parse_args()
 
+    os.makedirs(args.OUTPUT_DIR, exist_ok=True)
+
+    split_dir = 'qa_utils/splits'
     if args.DATA_SET == 'FIQA':
         args.FIQA_DIR = args.DATA_DIR
-        args.SPLIT_FILE = os.path.join(args.DATA_DIR, 'fiqa_split.pkl')
+        args.SPLIT_FILE = os.path.join(split_dir, 'fiqa_split.pkl')
         dataset = FiQA(args)
     elif args.DATA_SET == 'MSMARCO':
         args.MSM_DIR = args.DATA_DIR
+        args.MSM_SPLIT = os.path.join(split_dir, 'msm_split.pkl')
         dataset = MSMARCO(args)
     elif args.DATA_SET == 'INSURANCE_QA':
         args.INSRQA_V2_DIR = args.DATA_DIR
         dataset = InsuranceQA(args)
-    elif args.DATA_SET == 'WIKIPASSAGE_QA':
-        args.WPQA_DIR = args.DATA_DIR
-        args.SPLIT_FILE = os.path.join(args.DATA_DIR, 'wpqa_split.pkl')
-        dataset = WikiPassageQA(args)
+    elif args.DATA_SET == 'ANTIQUE':
+        args.ANTIQUE_DIR = args.DATA_DIR
+        args.SPLIT_FILE = os.path.join(split_dir, 'antique_split.pkl')
+        dataset = Antique(args)
     else:
         raise NotImplementedError()
 
